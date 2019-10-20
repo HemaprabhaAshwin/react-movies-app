d7a4355da3de25fbe75f99a25bc288cfa1f96b4e
HemaprabhaAshwin <hemaprabha.ashwin@gmail.com>
2019-10-20 18:57:10 +0400

    Added the Rating stars to the Details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
@@ -6,14 +6,45 @@ import Typography from '@material-ui/core/Typography';
 import './Details.css';
 import Home from '../../screens/home/Home';
 import Youtube from 'react-youtube';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 
-class Details extends Component{
+class Details extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            movie: {}
+            movie: {},
+            starIcons: [
+                {
+                   id: 1,
+                   stateId: "star1",
+                   color: "black"
+                },
+                {
+                   id: 2,
+                   stateId: "star2",
+                   color: "black"
+                },
+                {
+                   id: 3,
+                   stateId: "star3",
+                   color: "black"
+                },
+                {
+                   id: 4,
+                   stateId: "star4",
+                   color: "black"
+                },
+                {
+                   id: 5,
+                   stateId: "star5",
+                   color: "black"
+                }
+             ]
         }
     }
 
@@ -22,26 +53,42 @@ class Details extends Component{
         currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId
         })[0];
-        this.setState({currentState});
-        console.log(this.state);
-
+        this.setState({ currentState });
     }
 
     backToHomeHandler = () => {
         ReactDOM.render(<Home />, document.getElementById('root'));
     }
 
+    artistClickHandler = (url) => {
+        window.location = url;
+    }
+
+    starClickHandler = (id) => {
+        let starIconList =[];
+        for(let star of this.state.starIcons){
+            let starNode= star;
+            if(star.id <= id) {
+                starNode.color="yellow";
+            } else {
+                starNode.color ="black";
+            }
+            starIconList.push(starNode);
+        }
+        this.setState({starIcons: starIconList});
+    }
+
     render() {
         let movie = this.state.movie;
         const opts = {
             height: '300',
-            width:  '700',
+            width: '700',
             playerVars: {
                 autoplay: 1
             }
         }
-        return(
-            <div className="details"> 
+        return (
+            <div className="details">
                 <Header />
                 <div className="back">
                     <Typography onClick={this.backToHomeHandler}>
@@ -74,13 +121,37 @@ class Details extends Component{
                         <div className="trailerContainer">
                             <Typography><span className="bold">Trailer: </span></Typography>
                             <Youtube
-                               videoId={movie.trailer_url.split("?v=")[1]}
-                               opts={opts}
-                               onReady={this._onReady}
-                               />
+                                videoId={movie.trailer_url.split("?v=")[1]}
+                                opts={opts}
+                                onReady={this._onReady}
+                            />
                         </div>
                     </div>
                     <div className="rightDetails">
+                        <Typography><span className="bold">Rate this movie:</span></Typography>
+                        {this.state.starIcons.map(star =>(
+                            <StarBorderIcon className={star.color}  key={"star"+ star.id} onClick={this.starClickHandler(star.id)} />                      
+                        ))}
+                        <div className="bold marginBottom16 marginTop16">
+                            <Typography>
+                                <span className="bold">Artists:</span>
+                            </Typography>
+                        </div>
+                        <div className="paddingRight">
+                            <GridList cellHeight={160} cols={2}>
+                                {movie.artists != null && movie.artists.map(artist => (
+                                    <GridListTile
+                                        className="gridTile"
+                                        onClick={() => this.artistClickHandler(artist.wiki_url)}
+                                        key={artist.id}>
+                                        <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
+                                        <GridListTileBar
+                                            title={artist.first_name + " " + artist.last_name}
+                                        />
+                                    </GridListTile>
+                                ))}
+                            </GridList>
+                        </div>
                     </div>
                 </div>
             </div>
